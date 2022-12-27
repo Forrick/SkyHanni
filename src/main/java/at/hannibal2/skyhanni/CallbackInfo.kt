d28262ffcc22f41f14b5cc69138624b05a15a526
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2022-12-27 11:41:48 +0100

    removed all mixin stuff

--- src/main/java/at/hannibal2/skyhanni/CallbackInfo.kt
+++
@@ -1,7 +0,0 @@
-package at.hannibal2.skyhanni
-
-class CallbackInfo {
-    fun cancel() {
-
-    }
-}
--- src/main/java/at/hannibal2/skyhanni/events/DrawScreenAfterEvent.kt
+++
@@ -1,5 +0,0 @@
-package at.hannibal2.skyhanni.events
-
-import at.hannibal2.skyhanni.CallbackInfo
-
-class DrawScreenAfterEvent(val mouseX: Int, val mouseY: Int, val ci: CallbackInfo) : LorenzEvent()
